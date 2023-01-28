d408af9c0aa3d5577558508b73e1384285745b45
MattShannon <31073609+reejyinrussia@users.noreply.github.com>
2023-01-28 00:13:58 -0700

    and again

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Contacts API'
   },
-  host: 'week-02-personal.onrender.com/',
+  host: 'week-02-personal.onrender.com',
   schemes: ['http']
 };
 
